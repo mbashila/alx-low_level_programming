044c3d03b0b5a3da7aa7d08cde18eee0fcf8e9a7
mbashila <mbashilakuwunda@gmail.com>
2023-07-14 05:44:03 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main -Entery point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putd("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
