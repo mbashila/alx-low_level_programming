a2dd6eeb331d29157ea82e54422f97570c258dfa
mbashila <mbashilakuwunda@gmail.com>
2023-07-15 19:08:39 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	int n, m, l;
-	
+
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
@@ -15,9 +15,9 @@ int main(void)
 			{
 				if (l > m && m > n)
 				{
-					putchar(l);
 					putchar(n);
 					putchar(m);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
