570105b046f024ed06c975c15aae5e8fa1ffa8c4
mbashila <mbashilakuwunda@gmail.com>
2023-07-15 18:00:22 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int main(void)
 					putchar(l);
 					putchar(n);
 					putchar(m);
-					if (n !+= 55 || m != 56)
+					if (n != 55 || m != 56)
 					{
                                        		putchar(',');
                                         	pucthar(' ');
