d7c1c0de0f2b10f74b2d560ae37cdc6793cd5212
mbashila <mbashilakuwunda@gmail.com>
2024-01-18 03:04:27 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/mbashila/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -p .. https://raw.githubusercontent.com/mbashila/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
